79e5a8079517c97e66bf409e542dffe30e72a458
sofia <masocaucota@gmail.com>
2025-01-26 13:41:42 -0300

    error404-managed

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './error.css'
 
 const Error = () => {
   return (
-    <div>Error</div>
+    <div className='errorContainer'>
+      <div>
+        <h1>Error 404</h1>
+        <h3>Lo sentimos. Sitio no encontrado</h3>
+        <Link className='btn btnError' to='/'>Volver al Inicio</Link>
+      </div>
+    </div>
   )
 }
 
